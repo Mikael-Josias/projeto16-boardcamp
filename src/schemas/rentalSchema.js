a8485eba8c0036b023dd3afdd81f5422b4dd3f64
Mikael Josias <mikaeljosias2002@gmail.com>
2023-02-07 20:32:05 -0300

    feat: created a rent schema to validate rental informations

---
+++ src/schemas/rentalSchema.js
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+export const rentalSchema = Joi.object({
+    customerId: Joi.number().positive().integer().required(),
+    gameId: Joi.number().positive().integer().required(),
+    rentDate: Joi.date.required(),
+    daysRented: Joi.number().positive().integer().required(),
+    returnDate: Joi.date(),
+    originalPrice: Joi.number.positive().required(),
+    delayFee: Joi.number().positive(),
+});
+
