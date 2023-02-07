a14617dad132d28575503043fb9e6cd1b1dfb08f
Mikael Josias <mikaeljosias2002@gmail.com>
2023-02-07 11:38:45 -0300

    feat: created a customer schema to validate client informations

---
+++ src/schemas/customerSchema.js
@@ -0,0 +1,8 @@
+import Joi from "joi";
+
+export const customerSchema = Joi.object({
+    name: Joi.string().required(),
+    phone: Joi.string().min(10).max(11).pattern(/^\d+$/).required(),
+    cpf: Joi.string().length(11).pattern(/^\d+$/).required(),
+    birthday: Joi.date().required(),
+});
