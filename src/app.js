f59450e7baec39ce14a8348822e64e954d0e1948
Mikael Josias <mikaeljosias2002@gmail.com>
2023-02-08 20:09:03 -0300

    feat: added route to server

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ import cors from "cors";
 
 import chalk from "chalk";
 
+import gameRouter from "./routes/gameRouter.js";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (_, res) => {
-    res.send("Bem vindo ao BoardCamp 16");
-});
+app.use(gameRouter);
 
 app.listen(5000, () => {
     console.log(chalk.white.bgGreenBright("SERVER INICIALIZADO!"));
