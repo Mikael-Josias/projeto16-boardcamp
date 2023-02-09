10c4b6e5e03862cbf84680eb5551dc9122a0d320
Mikael Josias <mikaeljosias2002@gmail.com>
2023-02-09 10:19:07 -0300

    feat: created insert new customer route

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,10 +1,12 @@
 import { db } from "../database/database.connection.js";
+import { customerSchema } from "../schemas/customerSchema.js";
 
 import chalk from "chalk";
+import dayjs from "dayjs";
 
 export async function listAllCustomers(req, res) {
     try {
-        const customers = await db.query(`SELECT * FROM customers`);
+        const customers = await db.query(`SELECT * FROM customers;`);
         res.send(customers.rows);
     } catch (error) {
         console.log(chalk.redBright.white(error));
@@ -23,3 +25,26 @@ export async function getCustomerById(req, res) {
         res.sendStatus(500);
     }
 };
+
+export async function insertNewCustomer(req, res) {
+    try {
+        const data = {
+            name: req.body.name,
+            phone: req.body.phone,
+            cpf: req.body.cpf,
+            birthday: dayjs(req.body.birthday, "YYYY-MM-DD"),
+        };
+        if (customerSchema.validate().error) return res.sendStatus(400);
+
+        const customerAlreadyExists = await db.query(`SELECT cpf FROM customers WHERE cpf = $1;`, [data.cpf]);
+        if (customerAlreadyExists.rowCount !== 0) return res.sendStatus(409);
+
+        const result = await db.query(
+            `INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);`,
+            [data.name, data.phone, data.cpf, data.birthday]);
+        result.rowCount === 1 ? res.sendStatus(201) : res.sendStatus(400);
+    } catch (error) {
+        console.log(chalk.redBright.white(error));
+        res.sendStatus(500);
+    }
+}
--- src/routes/customerRouter.js
+++ src/routes/customerRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getCustomerById, listAllCustomers } from "../controllers/customerController.js";
+import { getCustomerById, insertNewCustomer, listAllCustomers } from "../controllers/customerController.js";
 
 const router = Router();
 
 router.get("/customers", listAllCustomers);
 router.get("/customers/:id", getCustomerById);
+router.post("/customers", insertNewCustomer);
 
 export default router;
