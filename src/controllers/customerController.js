809abe36a33fa4e900861cc7ad03ff35feb5f2c7
Mikael Josias <mikaeljosias2002@gmail.com>
2023-02-09 09:50:51 -0300

    feat: added get customer by id route

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -11,3 +11,15 @@ export async function listAllCustomers(req, res) {
         res.sendStatus(500);
     }
 };
+
+export async function getCustomerById(req, res) {
+    try {
+        const customerId = req.params.id;
+        
+        const customer = await db.query(`SELECT * FROM customers WHERE id = $1;`, [customerId]);
+        customer.rowCount === 0 ? res.sendStatus(404) : res.send(customer.rows);
+    } catch (error) {
+        console.log(chalk.redBright.white(error));
+        res.sendStatus(500);
+    }
+};
--- src/routes/customerRouter.js
+++ src/routes/customerRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { listAllCustomers } from "../controllers/customerController.js";
+import { getCustomerById, listAllCustomers } from "../controllers/customerController.js";
 
 const router = Router();
 
 router.get("/customers", listAllCustomers);
+router.get("/customers/:id", getCustomerById);
 
 export default router;
