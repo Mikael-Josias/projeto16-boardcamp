afc5e4e565193d18bdcdaf33b1ae53fb74d0d40c
Mikael Josias <mikaeljosias2002@gmail.com>
2023-02-08 21:28:52 -0300

    fix: changed game minimum stock

--- src/schemas/gameSchema.js
+++ src/schemas/gameSchema.js
@@ -3,6 +3,6 @@ import Joi from "joi";
 export const gameSchema = Joi.object({
     name: Joi.string().required(),
     image: Joi.string().required(),
-    stockTotal: Joi.number().integer().min(0).required(),
+    stockTotal: Joi.number().integer().positive().required(),
     pricePerDay: Joi.number().positive().required(),
 });
