cb1a384f372584046654842c43d661a774f5b0ed
Mikael Josias <mikaeljosias2002@gmail.com>
2023-02-09 11:14:31 -0300

    feat: created update customer informations route

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -50,3 +50,29 @@ export async function insertNewCustomer(req, res) {
         res.sendStatus(500);
     }
 };
+
+export async function updateCustomerData(req, res) {
+    try {
+        const customerId = req.params.id;
+        const data = {
+            name: req.body.name,
+            phone: req.body.phone,
+            cpf: req.body.cpf,
+            birthday: dayjs(req.body.birthday, "YYYY-MM-DD").toDate(),
+        };
+        if (customerSchema.validate(data).error) return res.sendStatus(400);
+
+        const cpfAlreadyUsed = await db.query(
+            `SELECT cpf FROM customers WHERE id <> $1 AND cpf = $2;`,
+            [customerId, data.cpf]);
+        if (cpfAlreadyUsed.rowCount !== 0) return res.sendStatus(409);
+
+        const result = await db.query(
+            `UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;`,
+            [data.name, data.phone, data.cpf, data.birthday, customerId]);
+        result.rowCount === 1 ? res.sendStatus(200) : res.sendStatus(400);
+    } catch (error) {
+        console.log(chalk.redBright.white(error));
+        res.sendStatus(500);
+    }
+};
--- src/routes/customerRouter.js
+++ src/routes/customerRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getCustomerById, insertNewCustomer, listAllCustomers } from "../controllers/customerController.js";
+import { getCustomerById, insertNewCustomer, listAllCustomers, updateCustomerData } from "../controllers/customerController.js";
 
 const router = Router();
 
 router.get("/customers", listAllCustomers);
 router.get("/customers/:id", getCustomerById);
 router.post("/customers", insertNewCustomer);
+router.put("/customers/:id", updateCustomerData);
 
 export default router;
