5d460283572a1aef857de4d910cd5b01250b4361
Mikael Josias <mikaeljosias2002@gmail.com>
2023-02-10 09:13:50 -0300

    fix: corrected syntax error

--- src/schemas/rentalSchema.js
+++ src/schemas/rentalSchema.js
@@ -3,10 +3,15 @@ import Joi from "joi";
 export const rentalSchema = Joi.object({
     customerId: Joi.number().positive().integer().required(),
     gameId: Joi.number().positive().integer().required(),
-    rentDate: Joi.date.required(),
+    rentDate: Joi.date().required(),
     daysRented: Joi.number().positive().integer().required(),
     returnDate: Joi.date(),
-    originalPrice: Joi.number.positive().required(),
+    originalPrice: Joi.number().positive().required(),
     delayFee: Joi.number().positive(),
 });
 
+export const insertRentalSchema = Joi.object({
+    customerId: Joi.number().positive().integer().required(),
+    gameId: Joi.number().positive().integer().required(),
+    daysRented: Joi.number().positive().integer().required(),
+});
