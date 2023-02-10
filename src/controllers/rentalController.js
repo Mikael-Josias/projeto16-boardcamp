7d33acc057bc646c727a15a21130a3ab3e4d50b0
Mikael Josias <mikaeljosias2002@gmail.com>
2023-02-10 09:15:28 -0300

    feat: created insert new rental route

--- src/controllers/rentalController.js
+++ src/controllers/rentalController.js
@@ -1,6 +1,8 @@
 import { db } from "../database/database.connection.js";
 
 import chalk from "chalk";
+import { insertRentalSchema } from "../schemas/rentalSchema.js";
+import dayjs from "dayjs";
 
 export async function listAllRentals(req, res) {
     try {
@@ -16,6 +18,32 @@ export async function listAllRentals(req, res) {
         res.send(rentals.rows);
     } catch (error) {
         console.log(chalk.redBright.white(error));
+        res.sendStatus(500);
+    }
+};
+
+export async function insertNewRental(req, res) {
+    try {
+        const { error, value } = insertRentalSchema.validate({
+            customerId: Number(req.body.customerId),
+            gameId: Number(req.body.gameId),
+            daysRented: Number(req.body.daysRented),
+        });
+        if (error) return res.sendStatus(400);
+        
+        const data = {
+            ...value,
+            rentDate: dayjs().toDate(),
+            returnDate: null,
+            delayFee: null,
+        };
+        
+        const result = await db.query(
+            `INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, $5, $4 * (select "pricePerDay" from games where id = $2), $6);`,
+            [data.customerId, data.gameId, data.rentDate, data.daysRented, data.returnDate, data.delayFee]);
+        result.rowCount !== 0 ? res.sendStatus(201) : res.sendStatus(400);
+    } catch (err) {
+        console.log(chalk.redBright.white(err));
         res.sendStatus(500);
     }
 };
--- src/routes/rentalRouter.js
+++ src/routes/rentalRouter.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { listAllRentals } from "../controllers/rentalController.js";
+import { insertNewRental, listAllRentals } from "../controllers/rentalController.js";
+import { verifyCustomerExists } from "../middleware/verifyCustomer.js";
+import { verifyGameExists } from "../middleware/verifyGame.js";
+import { verifyGameStock } from "../middleware/verifyGameStock.js";
 
 const router = Router();
 
 router.get("/rentals", listAllRentals);
+router.post("/rentals", verifyCustomerExists, verifyGameExists, verifyGameStock, insertNewRental);
 
 export default router;
